529b98737f291cdd7e4abf37d22e72630d0600db
mohana nr <@gmail.com>
2025-11-10 00:27:01 -0800

    products of healthy

---
+++ src/component/HealthyProducts.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Container from './Container'
+
+function HealthyProducts(props) {
+  return (
+    <div className='mt-10'>
+        <Container>
+            <div className='shadow-2xl rounded-2xl bg-white p-4'>
+                <figure className='ml-10'>
+                    <img className='rounded-2xl' src={props.img} alt="" />
+                </figure>
+                <h4 className='text-gray-700 font-bold text-xl lg:text-2xl mt-10 leading-relaxed whitespace-pre-line'>{props.name}</h4>
+                <p className='mt-5 text-sm md:text-md text-gray-700 leading-relaxed whitespace-pre-line'>{props.description1}</p>
+                <p className='mt-5 text-sm md:text-md text-gray-700 leading-relaxed whitespace-pre-line'>{props.description2}</p>
+                {
+                    props.description3 && (
+                        <>
+                            <p className='mt-5 text-sm md:text-md text-gray-700 leading-relaxed'>{props.description3}</p>
+                            <p className='mt-5 text-sm md:text-md text-gray-700 leading-relaxed'>{props.description4}</p>
+                            <p className='mt-5 text-sm md:text-md text-gray-700 leading-relaxed'>{props.description5}</p>
+                        </>
+                    )
+                }
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default HealthyProducts
